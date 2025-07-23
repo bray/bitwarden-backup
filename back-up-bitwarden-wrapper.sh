3f972586f880fc24431f0d3340014ce6fd42bdcc
Brian Ray <brian.ray1@gmail.com>
2025-07-22 22:46:43 -0400

    Add link to repo in wrapper script too

--- back-up-bitwarden-wrapper.sh
+++ back-up-bitwarden-wrapper.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# Repository: https://github.com/bray/bitwarden-backup
 # Bitwarden Vault Backup Script - Wrapper
 # SPDX-License-Identifier: MIT
 # Copyright (c) 2025 Brian Ray
